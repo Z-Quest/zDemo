437833da03ffe7adcfa8cc5c394d5dfecc364948
Finley <altimagewritingco@gmail.com>
2023-08-07 23:33:48 -0500

    Update and rename index.html to index.js
    Add template quest js

---
+++ ZQuestCore/index.js
@@ -0,0 +1,43 @@
+var quests = [];
+var questLog = [];
+var questTracker = {};
+
+function takeQuest() {
+  var quest = quests[Math.floor(Math.random() * quests.length)];
+  questLog.push(quest);
+  questTracker[quest.name] = 0;
+  console.log("You have taken the quest: " + quest.name);
+}
+
+function updateQuestTracker() {
+  for (var questName in questTracker) {
+    var quest = quests.find(function(quest) { return quest.name === questName });
+    questTracker[questName]++;
+    if (questTracker[questName] == quest.requirements) {
+      console.log("You have completed the quest: " + quest.name);
+      delete questTracker[questName];
+    }
+  }
+}
+
+function createQuest() {
+  var questName = prompt("Enter the name of your quest");
+  var questReward = prompt("Enter the reward for your quest");
+  var questDescription = prompt("Enter a description for your quest");
+  var questRequirements = prompt("Enter the requirements for completing your quest");
+
+  var quest = {
+    name: questName,
+    description: questDescription,
+    reward: questReward,
+    requirements: questRequirements
+  };
+
+  quests.push(quest);
+  console.log("Your quest has been created!");
+}
+
+document.querySelector("#take-quest").addEventListener("click", takeQuest);
+setInterval(updateQuestTracker, 1000);
+document.querySelector("#create-quest").addEventListener("click", createQuest);
+
